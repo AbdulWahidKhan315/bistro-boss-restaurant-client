b67f6f00091f6d9d790f23f8bda2664b4f667fc7
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-12 23:44:59 +0600

    added Popular Menu section for home page

---
+++ src/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import Menuitem from "../../components/MenuItem/Menuitem";
+
+const PopularMenu = () => {
+    const [menu,setMenu]=useState([]);
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popularMenu = data.filter(item => item.category === 'popular');
+            setMenu(popularMenu)
+        })
+    },[])
+    return (
+        <div className="mb-10">
+            <SectionTitle subHeading={'Check it out'} heading={'Popular Menu'}></SectionTitle>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {
+                    menu.map(item => <Menuitem key={item._id} menu={item}></Menuitem>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularMenu;
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className="md:my-8 w-3/12 mx-auto text-center">
-            <p className="text-yellow-600 mb-3">---{subHeading}---</p>
+            <p className="text-yellow-600 mb-3">--- {subHeading} ---</p>
             <h3 className="text-4xl border-y-4 py-4">{heading}</h3>
         </div>
     );
