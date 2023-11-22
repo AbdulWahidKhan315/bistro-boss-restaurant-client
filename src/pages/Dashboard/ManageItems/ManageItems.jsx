677f7a98ba6d7ad4fc8b6e2c5077ac400ba505c4
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-22 21:40:35 +0600

    added manageItems page for update and delete menu for admin

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,9 +1,97 @@
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure/useAxiosSecure";
 
 const ManageItems = () => {
+    const [menu, ,refetch]=useMenu();
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = (id) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${id}`);
+                if(res.data.deletedCount > 0){
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your item has been deleted",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+                console.log(res.data)
+            }
+          });
+    }
+
     return (
         <div>
             <SectionTitle heading={'manage all items'} subHeading={'Hurry Up!'}></SectionTitle>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    No.
+                                </th>
+                                <th>Item Image</th>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                menu.map((item,index)=> 
+                                <tr key={item._id}>
+                                <th>
+                                    {index+1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="no Image" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {
+                                        item.name
+                                    }
+                                </td>
+                                <td>{item.price}</td>
+                                <td>
+                                <button
+                                    className="btn bg-orange-500 text-white hover:text-orange-500"><FaEdit className="text-xl"></FaEdit></button>
+                                </td>
+                                <td>
+                                <button
+                                onClick={() => handleDelete(item._id)}
+                                className="btn btn-ghost"><FaTrashAlt className="text-red-500 text-xl"></FaTrashAlt></button>
+                                </td>
+                            </tr> )
+                            }
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
